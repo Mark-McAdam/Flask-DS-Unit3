027c9dae7b31c9605dac226bc697b22962e5acac
Mark McAdam <mark.a.mcadam@gmail.com>
2020-03-24 15:28:32 -0700

    twitoff models and app py updated

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -5,10 +5,17 @@ make the routes
 """
 
 from flask import Flask
+from .models import db
 
-# make app factory - reference the app.py file and the function inside
+#make app factory - reference the app.py file and the function inside
 def create_app():
     app = Flask(__name__)
+
+    #add config for the database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    #initialize database for the app - move to postgresql for the final format
+    db.init_app(app)
+
 
     @app.route('/')
     def root():
--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -6,13 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.model):
+class User(db.Model):
     """ Twitter users for analysis"""
     id = db.Column(db.BigInteger, primary_key=True)
     name = db.Column(db.String(15), nullable=False)
     
-class Tweet(db.model):
+class Tweet(db.Model):
     """ Tweets pulled for analysis"""
     id = db.Column(db.BigInteger, primary_key=True)
     text = db.Column(db.Unicode(280))
-    
