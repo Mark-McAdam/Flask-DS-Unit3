4f35af6a96d56d0220a1bc903695ee896958b497
Mark McAdam <mark.a.mcadam@gmail.com>
2020-03-24 17:48:41 -0700

    updated schema - models.py

--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -10,8 +10,16 @@ class User(db.Model):
     """ Twitter users for analysis"""
     id = db.Column(db.BigInteger, primary_key=True)
     name = db.Column(db.String(15), nullable=False)
-    
+    newest_tweet_id = db.Column(db.BigInteger)
+    def __repr__(self):
+        return f"<User {self.name}"
+
 class Tweet(db.Model):
     """ Tweets pulled for analysis"""
     id = db.Column(db.BigInteger, primary_key=True)
-    text = db.Column(db.Unicode(280))
+    text = db.Column(db.Unicode(300))
+    user_id = db.Column(db.BigInteger, db.ForeignKey('user.id'), nullable=False)
+    user = db.Column('User', backref=db.backref('tweets', lazy=True))
+    def __repr__(self):
+        return f"<Tweet {self.text}"
+
